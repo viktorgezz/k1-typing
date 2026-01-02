417392bca5997768979f337ba1d5de513d0001c3
 Viktor Gezz <viktorgezz@MacBook-Air-Viktor.local>
2026-01-02 13:43:22 +0300

    init commit

---
+++ k1-typing-backend/src/main/java/ru/viktorgezz/k1_typing_backend/K1TypingBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.viktorgezz.k1_typing_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K1TypingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K1TypingBackendApplication.class, args);
+	}
+
+}
